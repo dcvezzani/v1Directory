7db6ca2eaa67dc161e9b32dac59c164f41fe3288
David Vezzani <david.vezzani@greenseedtech.com>
2018-05-26 19:04:59 -0600

    adds contact processing for sending messages

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,30 @@ router.get('/format-email-addresses', function(req, res, next) {
   res.send(formattedAddresses.join(" "));
 });
 
+router.get('/contact-info', function(req, res, next) {
+	var data = req.query.data;
+	console.log(`contactInfo:`, req.query, data);
+
+	const parts = data.split(/:/);
+  res.json({lastname: parts[0], phone: parts[1], email: parts[2]});
+});
+
+router.post('/contact-visits', function(req, res, next) {
+	var lastname = Object.keys(req.body)[0];
+	var calendarEntries = req.body[lastname];
+	
+	const parts = calendarEntries.trim().split(/visit/);
+	let dateTimes = null;
+	_.each(parts, part => {
+		const jsonStr = part.replace(/^ ([^ ]+).*\nScheduled: ([^ ]+ [^,]+, [^ ]+) at ([^ ]+ [^ ]+).*$/gm, '{"lastname": "$1", "date": "$2", "time": "$3"}').trim();
+		if (jsonStr.length > 0) {
+			const entry = JSON.parse(jsonStr);
+			if (entry.lastname === lastname) {
+				dateTimes = entry;
+			}
+		}
+	});
+  res.json(dateTimes);
+});
+
 module.exports = router;
